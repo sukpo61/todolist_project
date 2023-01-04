dc054ffe610212d00653c1607622fdc1476128f2
go <sukpo6010@gmail.com>
2023-01-04 20:34:13 +0900

    Co-authored-by: 김상현 <arch-spatula@users.noreply.github.com>

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import TodoAddform from "./TodoAddform";
 import TodosContainer from "./TodosContainer";
 import styled from "styled-components";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { __getTodo } from "../redux/modules/todos";
 
 const TodoApp = () => {
@@ -20,6 +20,49 @@ const TodoApp = () => {
     </TodoAppWrap>
   );
 };
+
+// const createTodoKind = (...args) => {
+//   return {}
+// }
+
+// const shiftTodo = {
+//   todo: "progress",
+//   progress: "QA",
+//   QA: "Done",
+// }
+
+// const unshiftTodo = {
+//   progress: "todo",
+//   QA: "progress",
+//   Done: "QA",
+// }
+
+// const TodoItem = ({ id, kind, content, title }) => {
+//   const dispatch = useDispatch();
+//   const handleDelete = (id) => {
+//     dispatch(deleteTodoThunk(id));
+//   };
+//   const handleMove = (kind) => {
+//     const updateItem = {
+//       id,
+//       content,
+//       title,
+//       kind: shiftTodo[kind],
+//     };
+//     dispatch(moveTodoThunk(updateItem));
+//   };
+//   return (
+//     <div>
+//       <h3>{title}</h3>
+//       <p>{content}</p>
+//       <div>
+//         <button onClick={() => handleDelete(id)}>삭제</button>
+//         <button onClick={() => handleMove(kind)}>{shiftTodoText[kind]}</button>
+//         {/* 완료 : 취소 */}
+//       </div>
+//     </div>
+//   );
+// };
 
 const TodoAppWrap = styled.div`
   width: 70%;
